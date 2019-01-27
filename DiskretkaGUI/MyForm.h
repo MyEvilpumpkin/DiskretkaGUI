5e06b00eecf4423230ee492a6db3f271ecc8a12b
MyEvilpumpkin <dima.tomin2010@yandex.ru>
2019-01-27 11:11:43 +0500

    Настройка параметров основного окна

--- DiskretkaGUI/MyForm.h
+++ DiskretkaGUI/MyForm.h
@@ -48,11 +48,20 @@ namespace DiskretkaGUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(644, 379);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"MyForm";
+			this->Text = L"Ñomputer algebra system";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
